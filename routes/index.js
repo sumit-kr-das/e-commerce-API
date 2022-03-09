164d200e2bb0818d87184f024ec0bb6c0bea8034
sumit-kr-das <sumitkrdas777@gmail.com>
2022-03-10 00:30:43 +0530

    error in auth

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
 import express from "express";
 const routes = express.Router();
 
-import { 
-    registerController,
-    loginController
-} from '../controller';
-
+import { registerController, loginController } from "../controller";
 
 routes.post("/register", registerController.register);
 routes.post("/login", loginController.login);
-
 
 export default routes;
